feb85e80e4f80042529b4ce9c3d7215e6f06ee4d
Mike Manturov <insolor@users.noreply.github.com>
2014-10-09 16:11:56 +0300

    ataka() implemented

--- Grain.py
+++ Grain.py
@@ -670,7 +670,7 @@ def nn():
         key()
         cls()
     #1840 LET NAS=NAS-UMER+ROD
-    nas-=umer+rod
+    nas+=rod-umer
     #1850 RETURN 
 
 def war():
@@ -690,15 +690,37 @@ def war():
     #1900 RETURN 
 
 def ataka():
+    global nas, pogib, u, oi, rasst, vragi
     #1910 REM \#017\#001ATAKA\#017\#000
     #1920 RANDOMIZE USR VAL "54778": PRINT AT VAL "11",VAL "7";"\{i3}gorod atakowan!!!": GO SUB KEY: LET I=INT (INT (10*RND)*0.1)+0.5: LET J=I+INT (INT (10*RND)*0.15)+0.5
-    #1930 IF WRAGI>Z*J THEN PRINT AT VAL "13",BIN ;"gOROD PAL,WEDX EGO ZA]I]ALO MALO": PRINT AT VAL "14",VAL "13";"SOLDAT": GO SUB KEY: GO SUB CLS: LET U=SGN PI: LET OI=BIN : GO SUB OITOG: RETURN 
-    #1940 IF Z*J>WRAGI THEN PRINT AT VAL "13",INT PI;"NO ATAKA OTBITA! uRA-A-A!!!"
-    #1950 IF FN S(3)=SGN PI THEN PRINT AT VAL "15",INT PI;"pROIZO[LA RE[A\@]AQ BITWA!!!"
-    #1960 LET POGIBZ=INT (WRAGI/3)+INT (RND*Z/6): LET POGIB=POGIB+POGIBZ: LET NAS=NAS-POGIB: LET RASST=25+FN S(20): LET WRAGI=INT (NAS/4)+INT (RND*NAS/5)
-    #1965 GO TO KEY: GO SUB CLS
-    #1970 RETURN 
-    pass
+    print("ГОРОД АТАКОВАН!!!")
+    key()
+    i = int(int(10*random())*0.1)+0.5
+    j = i + int(int(10*random())*0.15)+0.5
+    #1930 IF WRAGI>Z*J THEN PRINT AT VAL "13",BIN ;"gOROD PAL,WEDX EGO ZA]I]ALO MALO": PRINT AT VAL "14",VAL "13";"SOLDAT": GO SUB KEY: GO SUB CLS: LET U=SGN PI: LET OI=BIN : GO SUB OITOG: RETURN
+    if vragi>z*j:
+        print("Город пал, ведь его защищало мало солдат")
+        key()
+        cls()
+        u=1
+        oi=0
+        oitog()
+        #1940 IF Z*J>WRAGI THEN PRINT AT VAL "13",INT PI;"NO ATAKA OTBITA! uRA-A-A!!!"
+    else:
+        print("но атака отбита! Ура-а-а!!!")
+        #1950 IF FN S(3)=SGN PI THEN PRINT AT VAL "15",INT PI;"pROIZO[LA RE[A\@]AQ BITWA!!!"
+        if fn_S(3)=1:
+            print("Произошла решающая битва!!!")
+        #1960 LET POGIBZ=INT (WRAGI/3)+INT (RND*Z/6): LET POGIB=POGIB+POGIBZ: LET NAS=NAS-POGIB: LET RASST=25+FN S(20): LET WRAGI=INT (NAS/4)+INT (RND*NAS/5)
+        pogibz=int(vragi/3)+int(random()*z/6)
+        pogib+=pogibz
+        nas-=pogib
+        rasst=25+fn_S(20)
+        vragi=int(nas/4)+int(random()*nas/5)
+        #1965 GO TO KEY: GO SUB CLS
+        key()
+        cls()
+        #1970 RETURN 
 
 def oitog():
     #1980 REM \#017\#001OITOG\#017\#000
