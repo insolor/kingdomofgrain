28fa6af683e48773d0502eb2ae6ceb751953e40f
insolor <insolor@gmail.com>
2020-10-24 00:35:38 +0300

    Move barter into separate module

---
+++ barter.py
@@ -0,0 +1,78 @@
+from abstract_screen import AbstractScreen
+from game_model import GameModel
+from grain import sub_input, pus, key
+
+
+def purchase_land(screen: AbstractScreen, model: GameModel):
+    while True:
+        # 510 LET ZEM=-1: PRINT AT VAL "20",VAL "5";"sKOLXKO KUPIM ZEMLI?"
+        zem = -1
+        screen.at(20, 5).print("Сколько купим земли?")
+
+        # 520 GO SUB INPUT:
+        #   LET ZEM=VAL F$:
+        #   GO SUB PUS:
+        #   IF ZEM<BIN THEN GO TO VAL "510"
+        zem = int(sub_input())
+        pus(screen)
+        if zem < 0:
+            continue
+
+        # 530 IF CENA*ZEM>ZERNO THEN PRINT AT VAL "20",BIN ;"u NAS TOLXKO ";ZERNO;" BU[ELEJ ZERNA!!!":
+        #   GO SUB KEY:
+        #   GO SUB PUS:
+        #   GO TO VAL "510"
+        if model.cena * zem > model.zerno:
+            screen.print(f"У нас только {model.zerno} бушелей зерна!!!")
+            key()
+            pus(screen)
+            continue
+
+        # 540 LET ZEML=ZEML+ZEM:
+        #   LET ZERNO=ZERNO-CENA*ZEM
+        model.zeml += zem
+        model.zerno -= model.cena * zem
+
+        # 550 PRINT AT VAL "20",SGN PI;"u NAS ";ZERNO;" BU[ELEJ ZERNA":
+        #   GO SUB KEY:
+        #   GO SUB PUS
+        screen.at(20, 1).print(f"У нас {model.zerno} бушелей зерна")
+        key()
+        pus(screen)
+        break
+
+
+def sell_land(screen: AbstractScreen, model: GameModel):
+    while True:
+        # 560 LET ZEM=-1:
+        #   PRINT AT VAL "20",VAL "4";"sKOLXKO ZEMLI PRODADIM?"
+        zem = -1
+        screen.at(20, 4).print("Сколько земли продадим?")
+
+        # 570 GO SUB INPUT: LET ZEM=VAL F$: IF ZEM<BIN THEN GO TO VAL "560"
+        zem = int(sub_input())
+        if zem < 0:
+            continue
+
+        # 580 IF ZEM>ZEML THEN PRINT AT VAL "20",BIN ;"u NAS TOLXKO ";ZEML;" AKROW ZEMLI!!!":
+        #   GO SUB KEY:
+        #   GO SUB PUS:
+        #   GO TO VAL "560"
+        if zem > model.zeml:
+            screen.at(20, 0).print(f"У нас только {model.zeml} акров земли!!!")
+            key()
+            pus(screen)
+            continue
+
+        # 590 LET ZEML=ZEML-ZEM: LET ZERNO=ZERNO+ZEM*CENA
+        model.zeml -= zem
+        model.zerno += zem * model.cena
+        # 600 RETURN
+        break
+
+
+def barter(screen: AbstractScreen, model: GameModel):
+    """Barter"""
+    # 500 REM \#017\#001TORGOWLQ\#017\#000
+    purchase_land(screen, model)
+    sell_land(screen, model)
--- grain.py
+++ grain.py
@@ -1,6 +1,7 @@
 from random import random
 
 from abstract_screen import AbstractScreen
+from barter import barter
 from game_model import GameModel
 
 # BAS file "" created by ZX-Modules
@@ -46,59 +47,6 @@ def fn_s(k):
 
 #  30 DIM Q(VAL "22")
 q = [0 for x in range(22)]
-
-
-def torg(model: GameModel):
-    # global zerno, zeml
-    # 500 REM \#017\#001TORGOWLQ\#017\#000
-    while True:
-        # 510 LET ZEM=-1: PRINT AT VAL "20",VAL "5";"sKOLXKO KUPIM ZEMLI?"
-        zem = -1
-        print("Сколько купим земли?")
-        # 520 GO SUB INPUT: LET ZEM=VAL F$: GO SUB PUS: IF ZEM<BIN THEN GO TO VAL "510"
-        zem = int(sub_input())
-        pus()
-        # 530 IF CENA*ZEM>ZERNO THEN PRINT AT VAL "20",BIN ;"u NAS TOLXKO ";ZERNO;" BU[ELEJ ZERNA!!!":
-        #   GO SUB KEY:
-        #   GO SUB PUS:
-        #   GO TO VAL "510"
-        if model.cena * zem > model.zerno:
-            print("У нас только %d бушелей зерна!!!" % model.zerno)
-            key()
-            pus()
-            continue
-        # 540 LET ZEML=ZEML+ZEM: LET ZERNO=ZERNO-CENA*ZEM
-        model.zeml += zem
-        model.zerno -= model.cena * zem
-        # 550 PRINT AT VAL "20",SGN PI;"u NAS ";ZERNO;" BU[ELEJ ZERNA":
-        #   GO SUB KEY:
-        #   GO SUB PUS
-        print("У нас %d бушелей зерна" % model.zerno)
-        key()
-        pus()
-        break
-
-    while True:
-        # 560 LET ZEM=-1: PR
-        #   NT AT VAL "20",VAL "4";"sKOLXKO ZEMLI PRODADIM?"
-        zem = -1
-        print("Сколько земли продадим?")
-        # 570 GO SUB INPUT: LET ZEM=VAL F$: IF ZEM<BIN THEN GO TO VAL "560"
-        zem = int(sub_input())
-        # 580 IF ZEM>ZEML THEN PRINT AT VAL "20",BIN ;"u NAS TOLXKO ";ZEML;" AKROW ZEMLI!!!":
-        #   GO SUB KEY:
-        #   GO SUB PUS:
-        #   GO TO VAL "560"
-        if zem > model.zeml:
-            print("У нас только %d акров земли!!!")
-            key()
-            pus()
-            continue
-        # 590 LET ZEML=ZEML-ZEM: LET ZERNO=ZERNO+ZEM*CENA
-        model.zeml -= zem
-        model.zerno += zem * model.cena
-        # 600 RETURN 
-        break
 
 
 def ohrana(model: GameModel):
@@ -1100,8 +1048,9 @@ def sub_input():
             return f
 
 
-def pus():
+def pus(screen: AbstractScreen):
     # 4030 PRINT AT VAL "20",BIN ;S$: RETURN
+    screen.print(s)
     pass
 
 
@@ -1163,7 +1112,7 @@ def main():
 
             # 170 IF U=NOT PI THEN GO SUB TORG
             if model.u is None:
-                torg(model)
+                barter(screen, model)
 
             # 180 IF U=NOT PI THEN GO SUB OHRANA
             if model.u is None:
