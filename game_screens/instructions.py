55b975cb2a40fbbf2f65d057844a434e26108358
insolor <insolor@gmail.com>
2020-10-24 22:03:25 +0300

    Minor fixes in instructions.py

--- game_screens/instructions.py
+++ game_screens/instructions.py
@@ -22,12 +22,12 @@ def instr(device: AbstractIO):
     #   PRINT "   ^TOBY ZASEQTX 1 AKR NUVNO":
     #   PRINT "   - 0.5 BU[ELEJ ZERNA":
     #   PRINT "   oDIN SOLDAT S'EDAET W GOD       - 5 BU[ELEJ ZERNA"
-    device.at(5, 8).ink(6).print(" Учтите, что:")
-    device.print("   Одному человеку нужно в год")
+    device.at(5, 8).ink(6).print(" Учтите что:")
+    device.print("  Одному человеку нужно в год")
     device.print("   не менее 22 бушелей зерна")
     device.print("   Чтобы засеять 1 акр нужно")
     device.print("   - 0.5 бушелей зерна")
-    device.print("  Один солдат съедает в год")
+    device.print("   Один солдат съедает в год")
     device.print("   - 5 бушелей зерна")
 
     # 1530 PRINT :
@@ -35,7 +35,7 @@ def instr(device: AbstractIO):
     device.print()
     device.print("        Желаем УСПЕХА!!!")
     # 1534 PRINT AT VAL "15",SGN PI;"\{i3} d-W REVIME \i\n\p PLAY SPECTRUM  WHOD W MEN\@           DOZAGRUZOK"
-    device.at(15, 1).ink(3).print(" d - в режиме play spectrum - вход в меню дозагрузок")
+    device.at(15, 1).ink(3).print(" d-в режиме INPUT - вход в меню\n           дозагрузок")
     # 1536 PRINT AT VAL "18",VAL "9";"\{i3}k-KONEC IGRY"
-    device.at(18, 9).ink(3).print(" k - конец игры")
+    device.at(18, 9).ink(3).print("k-конец игры")
     # 1540 RETURN
