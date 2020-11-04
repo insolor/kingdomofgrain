538816e5c876629ef93eb11a2327105a02083396
insolor <insolor@gmail.com>
2020-11-04 20:54:00 +0300

    Fix in war.py, some other minor fixes

--- game_screens/war.py
+++ game_screens/war.py
@@ -4,8 +4,6 @@ from io_devices import AbstractIO
 from game_model import GameModel
 from grain import sub_input, empty_lines, randint
 
-print = None
-
 
 def war(device: AbstractIO, model: GameModel):
     # 960 REM \#017\#001hAMONIQ\#017\#000
@@ -17,98 +15,113 @@ def war(device: AbstractIO, model: GameModel):
     else:
         # 980 PRINT AT VAL "19",SGN PI;"u NAS ";OST;" NEZANQTYH L\@DEJ":
         #   PRINT AT VAL "20",BIN ;"zERNA HWATIT NA ";INT (ZERNO/5);" WOINOW"
-        print("У нас %d незанятых людей" % ost)
-        print("Зерна хватит на %d воинов" % (zerno / 5))
+        device.at(12, 1).print(f"У нас {model.ost} незанятых людей")
+        device.at(20, 0).print(f"Зерна хватит на {model.zerno // 5} воинов")
         while True:
             # 990 LET POS=-1: PRINT AT VAL "21",INT PI;"sKOLXKO PO[LEM W hAMONI\@?"
-            pos = -1
-            print("Сколько пошлём в Хамонию?")
+            device.at(21, 1).print("Сколько пошлём в Хамонию?")
+
             # 1000 GO SUB INPUT:
             #   LET POS=VAL F$:
             #   PRINT AT VAL "18",BIN ;S$:
             #   GO SUB PUS:
             #   IF POS<BIN THEN GO TO VAL "990"
-            pos = int(sub_input())
+            try:
+                pos = int(sub_input(device, model))
+            except ValueError:
+                pos = -1
+
             device.at(18, 0).print(64 * " ")
             empty_lines(device)
+
             if pos < 0:
                 continue
+
             # 1010 IF POS>OST THEN
             #   PRINT AT VAL "20",VAL "5";"u NAS MALO L\@DEJ!!!":
             #   GO SUB KEY:
             #   GO SUB PUS:
             #   GO TO VAL "990"
-            if pos > ost:
-                print("У нас мало людей!!!")
-                key()
-                empty_lines()
+            if pos > model.ost:
+                device.at(20, 5).print("У нас мало людей!!!")
+                device.key()
+                empty_lines(device)
                 continue
+
             # 1020 IF POS>INT (ZERNO/5) THEN
             #   PRINT AT VAL "20",VAL "6";"u NAS MALO ZERNA!!!":
             #   GO SUB KEY:
             #   GO SUB PUS:
             #   GO TO VAL "990"
-            if pos * 5 > zerno:
-                print("У нас мало зерна!!!")
-                key()
-                empty_lines()
+            if pos * 5 > model.zerno:
+                device.at(20, 6).print("У нас мало зерна!!!")
+                device.key()
+                empty_lines(device)
                 continue
+
             break
+
     # 1030 IF POS<=NOT PI THEN GO TO VAL "1070"
     if pos > 0:
         # 1040 LET NAS=NAS-POS: LET OST=OST-POS: LET ZERNO=ZERNO-POS*5
-        nas -= pos
-        ost -= pos
-        zerno -= pos * 5
+        model.nas -= pos
+        model.ost -= pos
+        model.zerno -= pos * 5
+
         # 1050 IF ZAHW>BIN THEN LET ZAHW=ZAHW+POS: GO TO VAL "1070"
-        if zahv > 0:
-            zahv += pos
+        if model.zahv > 0:
+            model.zahv += pos
         else:
             # 1060 LET ZAHW=POS: LET SROK=TIME+FN S(4)
-            zahv = pos
-            srok = time + randint(4)
+            model.zahv = pos
+            model.srok = model.time + randint(4)
+
     # 1070 IF ZAHW<=NOT PI THEN GO TO VAL "1140"
-    if zahv <= 0:
-        pass
     # 1080 IF SROK<>TIME THEN GO TO VAL "1140"
-    elif srok != time:
-        pass
-    # 1090 IF ZAHW>NAS*VAL "2" THEN GO SUB POBEDA: GO TO VAL "1140"
-    elif zahv > nas * 2:
-        victory()
-    # 1100 IF ZAHW<INT (NAS/VAL "2") THEN GO SUB PORAV: GO TO VAL "1140"
-    elif zahv < nas // 2:
-        defeat()
-    # 1110 IF RND>0.5 THEN GO SUB POBEDA: GO TO VAL "1140"
-    elif random() > 0.5:
-        victory()
-    # 1120 GO SUB PORAV
-    else:
-        defeat()
+    if model.zahv > 0 and model.srok == model.time:
+        # 1090 IF ZAHW>NAS*VAL "2" THEN GO SUB POBEDA: GO TO VAL "1140"
+        if model.zahv > model.nas * 2:
+            victory(device, model)
+        # 1100 IF ZAHW<INT (NAS/VAL "2") THEN GO SUB PORAV: GO TO VAL "1140"
+        elif model.zahv < model.nas // 2:
+            defeat(device, model)
+        # 1110 IF RND>0.5 THEN GO SUB POBEDA: GO TO VAL "1140"
+        elif random() > 0.5:
+            victory(device, model)
+        # 1120 GO SUB PORAV
+        else:
+            defeat(device, model)
+
     # 1140 RETURN
 
 
 def defeat(device: AbstractIO, model: GameModel):
     # 1240 REM \#017\#001PORAVENIE\#017\#000
     # 1245 GO SUB CLS: RANDOMIZE USR VAL "54778"
-    cls()
+    device.cls()
+
     # 1250 PRINT AT VAL "11",VAL "5";"\{i3}poravenie w hAMONII!!!"
-    print("ПОРАЖЕНИЕ В Хамонии!!!")
+    device.at(11, 5).ink(3).print("ПОРАЖЕНИЕ В Хамонии!!!")
+
     # 1260 LET UBITO=INT (ZAHW*(0.8+RND/5)):
     #   LET NAS=NAS+ZAHW-UBITO:
     #   LET POGIB=POGIB+UBITO
-    ubito = int(zahv * (0.8 + random() / 5))
-    nas += zahv - ubito
-    pogib += ubito
+    ubito = int(model.zahv * (0.8 + random() / 5))
+    model.nas += model.zahv - ubito
+    model.pogib += ubito
+
     # 1270 PRINT AT VAL "13",SGN PI;"pOGIBLO - ";UBITO;" WOINOW"
-    print("Погибло - %d воинов" % ubito)
+    device.at(13, 1).print(f"Погибло - {ubito} воинов")
+
     # 1280 LET ZAHW=BIN :
     #   LET OST=NAS-Z-INT (ZAS/PROIZ)
-    zahv = 0
-    ost = nas - z - int(zas / proiz)
+    model.zahv = 0
+    model.ost = model.nas - model.z - model.zas // model.proiz
+
     # 1285 GO SUB KEY: GO SUB CLS
-    key()
-    cls()
+    device.key()
+    device.cls()
+
     # 1290 RETURN
 
 
@@ -116,42 +129,52 @@ def victory(device: AbstractIO, model: GameModel):
     # 1150 REM \#017\#001POBEDA\#017\#000
     # 1155 GO SUB CLS: RANDOMIZE USR VAL "54778"
     device.cls()
+
     # 1160 PRINT AT VAL "11",VAL "2";"\{i6}w hAMONII ODERVANA pobeda!!!"
-    print("В хамонии одержана победа!!!")
+    device.at(11, 2).ink(6).print("В хамонии одержана победа!!!")
+
     # 1170 LET T(INT PI)=INT (NAS*(RND/3+0.3)):
     #   LET T(SGN PI)=INT (ZAHW*(FN S(VAL "10")+VAL "4")):
     #   LET T(VAL "2")=INT (ZEML*(RND/VAL "2"+0.3)):
     #   LET NAS=NAS+T(INT PI):
     #   LET ZEML=ZEML+T(VAL "2"):
     #   LET ZERNO=ZERNO+T(SGN PI)
-    captured_people = int(nas * (random() / 3 + 0.3))
-    taken_grain = int(zahv * randint(10) + 4)
-    annexed_territory = int(zeml * (random() / 2 + 0.3))
-    nas += captured_people
-    zeml += annexed_territory
-    zerno += taken_grain
+
+    captured_people = int(model.nas * (random() / 3 + 0.3))
+    taken_grain = int(model.zahv * randint(10) + 4)
+    annexed_territory = int(model.zeml * (random() / 2 + 0.3))
+
+    model.nas += captured_people
+    model.zeml += annexed_territory
+    model.zerno += taken_grain
 
     # 1180 LET UBITO=INT (ZAHW*(RND/5+0.3)):
     #   LET POGIB=POGIB+UBITO:
     #   LET NAS=NAS+ZAHW-UBITO
-    ubito = int(zahv*(random() / 5 + 0.3))
-    pogib += ubito
-    nas += zahv - ubito
+    ubito = int(model.zahv * (random() / 5 + 0.3))
+    model.pogib += ubito
+    model.nas += model.zahv - ubito
+
     # 1190 PRINT AT VAL "13",BIN ;"u WRAGA ZAHWA^ENO:"
-    print("У врага захвачено:")
+    device.at(13, 0).print("У врага захвачено:")
+
     # 1200 PRINT AT VAL "14",SGN PI;"zERNA - ";T(1);" BU[ELEJ":
     #   PRINT " zEMLI - ";T(2);" AKROW": PRINT " pLENNYH - ";T(3);" ^ELOWEK"
-    print("Зерна - %d бушелей" % taken_grain)
-    print("Земли - %d акров" % annexed_territory)
-    print("Пленных - %d человек" % captured_people)
+    device.at(14, 1).print(f"Зерна - {taken_grain} бушелей")
+    device.print(f"Земли - {annexed_territory} акров")
+    device.print(f"Пленных - {captured_people} человек")
+
     # 1210 PRINT AT VAL "18",VAL "6";"iZ ";ZAHW;" WOINOW ";UBITO;AT VAL "19",VAL "10";"PALO SMERTX\@ HRABRYH!"
-    print("Из %d воинов %d пало сметрью храбрых!" % (zahv, ubito))
+    device.at(18, 6).print(f"Из {model.zahv} воинов {ubito} пало сметрью храбрых!")
+
     # 1220 LET ZAHW=BIN :
     #   LET OST=NAS-Z-INT (ZAS/PROIZ)
-    zahv = 0
-    ost = nas - z - int(zas / proiz)
+    model.zahv = 0
+    model.ost = model.nas - model.z - int(model.zas / model.proiz)
+
     # 1225 GO SUB KEY:
     #   GO SUB CLS
-    key()
-    cls()
+    device.key()
+    device.cls()
+
     # 1230 RETURN
