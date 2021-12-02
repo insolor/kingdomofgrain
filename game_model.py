6dfe5d39cb1877af27319f6c46688ef974099cb2
insolor <insolor@gmail.com>
2021-12-02 14:38:49 +0300

    Fixes in the game model

--- game_model.py
+++ game_model.py
@@ -9,26 +9,7 @@ def fn_s(k):
 
 @dataclass
 class GameModel:
-    #  90 LET UMER=NOT PI:
-    #   LET ROD=NOT PI:
-    #   LET UMERGOL=NOT PI:
-    #   LET UMERWSEGO=NOT PI:
-    #   LET ZAHW=NOT PI:
-    #   LET POGIB=NOT PI:
-    #   LET PROIZ=VAL "10":
-    #   LET PREDEL=VAL "15"
-
-    defenders: int = 0
-    dead_natural_cases: int = 0
-    born: int = 0
-    dead_starvation: int = 0  # dead from starvation
-    dead_total: int = 0  # dead total
-
-    warriors: int = 0  # number of our warriors
-    dead_in_battles: int = 0  # dead in battle
-
-    sower_productivity: int = 10  # productivity of a sower (acres)
-    max_sower_productivity: int = 15  # maximum productivity of a sower
+    defenders: int
 
     # 100 LET WRAGI=FN S(VAL "10")+VAL "25":
     #   LET RASST=FN S(VAL "10")+VAL "25":
@@ -53,22 +34,46 @@ class GameModel:
     #   LET AGENT=NOT PI:
     #   LET NBOG=CENA*ZEML+ZERNO:
     #   LET IST=NOT PI
-    grain_yield: int = harvest // land  # yield of grain per acre of land
+    grain_yield: int = -1  # yield of grain per acre of land
     rats: int = field(default_factory=lambda: 150 + fn_s(200))  # rats (grain eaten by rats)
-    grain: int = harvest - rats  # grain left
-    time: int = 1  # number of a year
+    grain: int = -1  # grain left
+    time: int = 1  # current year
 
     refugees: int = field(default_factory=lambda: 5 + fn_s(5))  # refugees income
-    agent: int = 0  # enemy agent
-    ist: int = 0
+    agent: int = 0  # enemy agent flag  # FIXME: specify possible values
+    ist: int = 0  # TODO: restore meaning of the field
 
     feeding_per_worker: Optional[int] = None  # Quantity of grain per worker to feed one
     ost: Optional[int] = None  # unoccupied population
     zas: Optional[int] = None  # sowed area
     initial_wealth: Optional[int] = 0
 
+    # Fields where the game stats are stored
+
+    #  90 LET UMER=NOT PI:
+    #   LET ROD=NOT PI:
+    #   LET UMERGOL=NOT PI:
+    #   LET UMERWSEGO=NOT PI:
+    #   LET ZAHW=NOT PI:
+    #   LET POGIB=NOT PI:
+    #   LET PROIZ=VAL "10":
+    #   LET PREDEL=VAL "15"
+
+    dead_natural_cases: int = 0
+    born: int = 0
+    dead_starvation: int = 0  # dead from starvation
+    dead_total: int = 0  # dead total
+
+    warriors: int = 0  # number of our warriors
+    dead_in_battles: int = 0  # dead in battle
+
+    sower_productivity: int = 10  # productivity of a sower (acres)
+    max_sower_productivity: int = 15  # maximum productivity of a sower
+
     def __post__init__(self):
         self.initial_wealth = self.wealth()
+        self.grain_yield = self.harvest // self.land
+        self.grain = self.harvest - self.rats
 
     def wealth(self):
         return self.land_price * self.land + self.grain
