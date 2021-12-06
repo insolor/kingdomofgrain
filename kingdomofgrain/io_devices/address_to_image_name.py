279beebb5fa0f746d7ece510b3ce1b9a3d3ca1f0
insolor <insolor@gmail.com>
2021-12-06 09:44:43 +0300

    Fix typo in "locust"

--- kingdomofgrain/io_devices/address_to_image_name.py
+++ kingdomofgrain/io_devices/address_to_image_name.py
@@ -12,7 +12,7 @@ address_to_image_name = {
     57094: "rats",
     59410: "rains",
     60568: "dust_storms",
-    61726: "locrust",
+    61726: "locust",
     62884: "bad_harvest",
     46225: "baby_boom",
     64042: "game_end",
