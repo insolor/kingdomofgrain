0b11bf5ea82da2af0115f42bc861d04a704646f7
insolor <insolor@gmail.com>
2023-07-28 23:29:52 +0300

    Create device, run the game in a separate thread

--- kingdomofgrain/pyxel_app.py
+++ kingdomofgrain/pyxel_app.py
@@ -1,9 +1,16 @@
+from threading import Thread
+
 import pyxel
+
+from kingdomofgrain import grain
+from kingdomofgrain.io_devices.pyxel_io import PyxelIO
 
 
 class App:
     def __init__(self):
         pyxel.init(256, 192)
+        self.device = PyxelIO()
+        Thread(target=grain.main, args=(self.device,)).run()
 
     def update(self):
         pass
